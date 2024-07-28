73f0b35bf640cefdd02872e1e21b57d2fd6e7e2f
Y1K8M2NT1RK <koutarou0211@icloud.com>
2024-07-28 17:56:48 +0900

    端末のデザインモードに応じて、アプリケーションのデザインモードを設定できるよう修正

--- polygonal-auxions/src/pages/_app.tsx
+++ polygonal-auxions/src/pages/_app.tsx
@@ -3,29 +3,27 @@ import type { AppProps } from 'next/app';
 import { cacheExchange, fetchExchange, createClient, Provider as UrqlProvider } from 'urql';
 import { AppCacheProvider as MUIProvider } from '@mui/material-nextjs/v14-pagesRouter';
 import { SessionProvider } from 'next-auth/react';
-import { ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
 import Header from '@/pages/components/Header';
 import NextTopLoader from 'nextjs-toploader';
 
-// ダークモードの設定
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 export default function App({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
+  // 端末のデザインモードに応じてサイトのデザインモードを設定
+  const theme = createTheme({
+    palette: {mode: useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light'},
+  });
   const urqlClient = createClient({
     exchanges: [cacheExchange, fetchExchange],
     url: 'http://localhost:3001/api/graphql',
   });
   return (
     <MUIProvider>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <UrqlProvider value={urqlClient}>
           <SessionProvider session={session}>
             <NextTopLoader
-              color="#AAAAAA"
+              color={theme.palette.mode=="dark"?"#AAAAAA":"666666"}
               initialPosition={0.08}
               crawlSpeed={200}
               height={5}
