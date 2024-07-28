349585eb417024339d0118cda8184fe78275d30a
Y1K8M2NT1RK <koutarou0211@icloud.com>
2024-07-29 00:19:35 +0900

    プロフィール関係のGraphQLクエリにおいて、下記の通りに対応 ・prismaObjectを他のGraphQLクエリ/ミューテーションでも使用できるように修正 ・prismaObjectのFieldに「slug_id」を追加 ・未使用の引数は先頭に「_」を追加 ・改行が少ない箇所を１行に集約

--- polygonal-auxions/src/pages/api/types/queries/users.ts
+++ polygonal-auxions/src/pages/api/types/queries/users.ts
@@ -2,9 +2,10 @@ import { builder } from '../../builder';
 import { prisma } from '../../db';
 
 // メインのテーブル
-builder.prismaObject('User', {
+export const User = builder.prismaObject('User', {
   fields: (t) => ({
     name: t.exposeString('name'),
+    slug_id: t.exposeString('slug_id'),
     name_kana: t.exposeString('name_kana', {nullable: true}),
     handle_name: t.exposeString('handle_name'),
     email: t.exposeString('email'),
@@ -19,14 +20,12 @@ builder.prismaObject('User', {
 // ユーザープロフィール
 builder.queryField("user", (t) =>
   t.prismaField({
-    type: "User", // 一つのデータを求める場合は[]で囲わない
-    args: {
-      handle_name: t.arg.string({ required: true }),
-    },
-    resolve: (query, _parent, _args, _ctx, _info) =>
+    type: User,
+    args: { handle_name: t.arg.string({ required: true }), },
+    resolve: (query, _parent, args, _ctx, _info) =>
       prisma.user.findUniqueOrThrow({
         ...query,
-        where: { handle_name: _args.handle_name },
+        where: { handle_name: args.handle_name },
         include: {
           artworks: { orderBy: { created_at : 'desc' }},
           comments: { orderBy: { created_at : 'desc' }}
