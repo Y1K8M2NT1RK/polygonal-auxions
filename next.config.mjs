60e039a10fcc435d2ed68a81697f1520ccc79899
Y1K8M2NT1RK <koutarou0211@icloud.com>
2025-04-23 00:07:24 +0900

    デプロイ準備（2025/04/23）

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
             maxSize: 250000,
         };
         return config;
+    },
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}/api/graphql`
+            : 'http://localhost:3000/api/graphql',
     }
 };
 
