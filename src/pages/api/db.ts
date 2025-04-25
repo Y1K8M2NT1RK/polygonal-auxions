3af952ba26bd1af5387a8e486d86f2baca0d99ef
Y1K8M2NT1RK <koutarou0211@icloud.com>
2025-04-26 01:03:26 +0900

    デプロイ準備2（2025/04/26）

--- src/pages/api/db.ts
+++ src/pages/api/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '.prisma/client/index.js';
+import { PrismaClient } from "../../../prisma/generated/client";
 
 declare global {
     var prisma: PrismaClient | undefined
