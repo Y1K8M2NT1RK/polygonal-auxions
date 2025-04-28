fe63af46c4f6355498827f0cb51f84bcb2cd00f6
Y1K8M2NT1RK <koutarou0211@icloud.com>
2025-04-28 11:21:27 +0900

    Preparingコンポーネントのインデントを修正

--- src/components/Preparing.tsx
+++ src/components/Preparing.tsx
@@ -5,21 +5,21 @@ import React from 'react';
 export default function Preparing() {
     return (
         <Container
-        sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '80vh',
-        }}
-        >
-        <Box
             sx={{
-                textAlign: 'center',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '80vh',
             }}
         >
-            <ConstructionIcon sx={{ fontSize: 40 }} />
-            <Typography variant="h6">準備中...</Typography>
-        </Box>
+            <Box
+                sx={{
+                    textAlign: 'center',
+                }}
+            >
+                <ConstructionIcon sx={{ fontSize: 40 }} />
+                <Typography variant="h6">準備中...</Typography>
+            </Box>
         </Container>
     );
 }
