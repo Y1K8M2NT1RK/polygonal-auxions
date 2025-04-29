449b2efae55bc17b6bcc61f3d8033e3227b2d52b
Y1K8M2NT1RK <koutarou0211@icloud.com>
2025-04-29 19:16:47 +0900

    ・本番環境に限り、下記のパスにアクセスした場合は404エラーを返すよう修正 　・"/api/debug" 　・"/api/openssl" ・コメント追加

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+    // Basic Authによる認証
     const auth = request.headers.get('authorization');
     if (auth) {
         const [scheme, encoded] = auth.split(' ');
@@ -10,6 +11,13 @@ export function middleware(request: NextRequest) {
             if (user === process.env.BASIC_AUTH_USER && pass === process.env.BASIC_AUTH_PASS) {
                 return NextResponse.next();
             }
+        }
+    }
+    // 本番環境で"/api/debug"と"/api/openssl"にアクセスした場合は404エラーを返す
+    if( process.env.NODE_ENV === 'production' ) {
+        const restrictedPaths = ['/api/debug', '/api/openssl'];
+        if (restrictedPaths.some(path => request.nextUrl.pathname.startsWith(path))) {
+            return NextResponse.json({error: 'Not Found'},  {status: 404});
         }
     }
     return new Response('Authentication required', {
