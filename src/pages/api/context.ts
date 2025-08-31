0fc8a1f1c9938c7077e6159ddd1f8bf0d33b0af2
Y1K8M2NT1RK <91308129+Y1K8M2NT1RK@users.noreply.github.com>
2025-08-31 15:24:53 +0000

    fix(api): context.ts に default export 追加

--- src/pages/api/context.ts
+++ src/pages/api/context.ts
@@ -5,3 +5,8 @@ export interface YogaContext {
   req: NextApiRequest;
   auth: any;
 }
+
+// 型検証で API Route と見なされ default export 無しでエラーとなるためダミーを追加
+export default function _unusedContextEndpoint(_req: NextApiRequest, res: NextApiResponse) {
+  res.status(405).json({ error: 'Unused context module' });
+}
