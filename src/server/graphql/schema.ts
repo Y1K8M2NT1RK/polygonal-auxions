1b42527ef3539ac52553da02eb404a91ef704933
Y1K8M2NT1RK <91308129+Y1K8M2NT1RK@users.noreply.github.com>
2025-08-31 15:43:00 +0000

    fix(build): schema.ts の型定義インポートを相対パス化 (jiti解決対策)

--- src/server/graphql/schema.ts
+++ src/server/graphql/schema.ts
@@ -1,12 +1,12 @@
 import { builder } from './builder';
 
-import '@/pages/api/types/errors';
-import '@/pages/api/types/queries/artworks';
-import '@/pages/api/types/mutations/artworks';
-import '@/pages/api/types/queries/users';
-import '@/pages/api/types/mutations/users';
-import '@/pages/api/types/mutations/emails';
-import '@/pages/api/types/queries/comments';
-import '@/pages/api/types/mutations/comments';
+import '../../pages/api/types/errors';
+import '../../pages/api/types/queries/artworks';
+import '../../pages/api/types/mutations/artworks';
+import '../../pages/api/types/queries/users';
+import '../../pages/api/types/mutations/users';
+import '../../pages/api/types/mutations/emails';
+import '../../pages/api/types/queries/comments';
+import '../../pages/api/types/mutations/comments';
 
 export const schema = builder.toSchema();
